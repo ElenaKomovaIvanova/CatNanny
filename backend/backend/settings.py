013aef8e560acd983bd7a0a68979247b3540ca42
Elena Komova <e.komova01@gmail.com>
2024-11-11 17:01:47 +0200

    add heroku

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -164,7 +164,7 @@ CORS_ALLOWED_ORIGINS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
